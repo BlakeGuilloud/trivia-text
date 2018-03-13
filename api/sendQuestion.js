34f0dd03f905ccf1c5e675048e9830c2aa21d152
Blake Guilloud <blake.guilloud@gmail.com>
2018-03-13 14:18:58 -0400

    Blake - add "he" package for html decoding.

--- api/sendQuestion.js
+++ api/sendQuestion.js
@@ -2,13 +2,14 @@
 
 const axios = require('axios');
 const { handleError, handleSuccess, tryParse } = require('serverless-helpers/responses');
+const he = require('he');
 
 const sendMessage = require('./utils/sendMessage');
 
 module.exports.handler = (event, context, callback) => {
   return axios.get('https://opentdb.com/api.php?amount=1&type=multiple')
     .then(({ data}) => data.results[0])
-    .then(({ question }) => sendMessage({ to: '+18438126962', body: question }))
+    .then(({ question }) => sendMessage({ to: '+18438126962', body: he.decode(question) }))
     .then(() => callback(null, handleSuccess('Question sent successfully')))
     .catch(err => callback(null, handleError(err)));
 };
